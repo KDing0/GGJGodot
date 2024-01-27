d0fb1edf2d92bf2444c26253062750147f09ed3b
Jonas Seidl <jonasseidl2013@gmail.com>
2024-01-27 08:57:20 +0100

    Added menu

---
+++ src/Misc/PauseMenu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+signal resumeGame
+signal toMenu
+signal musicChanged(newVal)
+signal sfxChanged(newVal)
+
+func _on_btn_resume_game_pressed():
+	resumeGame.emit()
+
+func _on_btn_to_menu_pressed():
+	toMenu.emit()
+	
+func _on_slider_music_value_changed(value):
+	musicChanged.emit(value)
+	
+func _on_slider_sfx_value_changed(value):
+	sfxChanged.emit(value)
+
+func setMusicAndSFX(val_music: float, val_sfx: float):
+	$MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/Slider_Music.value = val_music
+	$MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer2/Slider_SFX.value = val_sfx
