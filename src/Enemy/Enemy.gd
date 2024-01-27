efbbf9b04ae16e4166758d64318ea83376e15212
KDing0 <38357906+KDing0@users.noreply.github.com>
2024-01-27 14:22:54 +0100

    Enemy Bullet Spawn

--- src/Enemy/Enemy.gd
+++ src/Enemy/Enemy.gd
@@ -1,6 +1,10 @@
 extends PathFollow2D
 
 @export var SPEED = 5.0
+@export var BulletSpawnID = "one"
+@export var BulletAnimationID = "first"
+@export var BulletRotation = 15
+@export var BulletSpawnOffset = Vector2(0,0)
 
 func _process(delta):
 	progress_ratio += SPEED / 1000.0
@@ -8,6 +12,12 @@ func _process(delta):
 func _on_timer_timeout():
 	# shoot()
 	pass
+
+func _input(event):
+	if event.is_action_pressed("PTest"):
+		print("triggered")
+		var pos = global_position + BulletSpawnOffset
+		Spawning.spawn({"position": pos, "rotation": 15}, "one", "first")
 
 func shoot():
 	# for anzahl bullets
