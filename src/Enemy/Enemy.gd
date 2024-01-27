2074469852d9eac0b791b7c445a3c8eac78a6c5e
Jonas Seidl <jonasseidl2013@gmail.com>
2024-01-27 14:21:06 +0100

    Added enemy spawning in GameController

--- src/Enemy/Enemy.gd
+++ src/Enemy/Enemy.gd
@@ -1,9 +1,22 @@
 extends PathFollow2D
+class_name Enemy
 
-@export var SPEED = 5.0
+enum EnemyTypes {ENEMY_TYPE1, ENEMY_TYPE2}
+
+var speed = 5.0
+
+func set_parameters(p_texture: Texture2D = null, p_speed: float = 5.0) -> void:
+	if p_texture:
+		$Sprite2D.texture = p_texture
+	speed = p_speed
+	
+	self.rotates = false
+	self.loop = false
 
 func _process(delta):
-	progress_ratio += SPEED / 1000.0
+	progress_ratio += speed / 1000.0
+	if progress_ratio >= 1.0:
+		self.queue_free()
 
 func _on_timer_timeout():
 	# shoot()
--- src/Misc/GameController.gd
+++ src/Misc/GameController.gd
@@ -1,5 +1,13 @@
 extends Node
 
+@export var tex_enemy : Array[Texture2D]
+
+@export
+
+var test = 1.0
+var d = 0.0
+
+var enemyScene = preload("res://Enemy/enemy.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +16,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	d += delta
+	if d > 1.0:
+		spawnEnemy(Enemy.EnemyTypes.ENEMY_TYPE1)
+		d = 0.0
+
+
+func spawnEnemy(enemyType):
+	var enemy_instance = enemyScene.instantiate()
+	enemy_instance.set_parameters(tex_enemy[0], test)
+	test += 1.0
+	get_node("../EnemyPaths/Path2D").add_child(enemy_instance)
+	
