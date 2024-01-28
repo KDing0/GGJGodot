69d86f384521edd48e2cc473f7e2adfea68f0253
Anna Ricker <anna.ricker8@gmail.com>
2024-01-28 10:28:01 +0100

    change lives to hearts

--- src/UI/lives.gd
+++ src/UI/lives.gd
@@ -1,16 +1,17 @@
 extends CanvasLayer
 
 signal game_over
+var empty_heart = preload("res://Assets/UI/heart_empty.png")
 
 func _ready():
 	Livecounter.lives=3
 
 func _process(delta):
 	if Livecounter.lives==2:
-		$HBoxContainer/Sprite2D3.visible = false
+		$HBoxContainer/Sprite2D.texture = empty_heart
 	if Livecounter.lives==1:
-		$HBoxContainer/Sprite2D2.visible = false
+		$HBoxContainer/Sprite2D2.texture = empty_heart
 	if Livecounter.lives==0:
-		$HBoxContainer/Sprite2D.visible = false
+		$HBoxContainer/Sprite2D.texture = empty_heart
 		game_over.emit()
 		#get_tree().reload_current_scene()
