5431788a4d4323ee4255120116d1847fcf7ee7f3
Jonas Seidl <jonasseidl2013@gmail.com>
2024-01-28 12:10:35 +0100

    Added word hitting functionality

--- src/Textboxes/TextCollisions.gd
+++ src/Textboxes/TextCollisions.gd
@@ -4,8 +4,13 @@ extends Node2D
 @export var textbox: CanvasLayer
 
 func _ready():
-	self.add_child(textbox.add_area_on("Bla"))
+	var textHitArea = textbox.add_area_on("Test")
+	textHitArea.hit.connect(_on_text_hit)
+	self.add_child(textHitArea)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position = Vector2.ZERO - cam.position
+	self.position = Vector2.ZERO + cam.position
+
+func _on_text_hit(from, to):
+	textbox.changeText(from, to)
--- src/Textboxes/textbox.gd
+++ src/Textboxes/textbox.gd
@@ -2,10 +2,15 @@ extends CanvasLayer
 
 @onready var anims: AnimationPlayer = $AnimationPlayer
 
+var area_script = load("res://Textboxes/textbox_areas.gd")
 var textbox_open = true
 
 func _ready():
 	#open_textbox()
+	$Control.offset_bottom = 0.0
+	$Control.offset_left = 0.0
+	$Control.offset_right = 0.0
+	$Control.offset_top = 0.0
 	pass
 
 func open_textbox():
@@ -21,13 +26,42 @@ func close_textbox():
 func setText(text):
 	pass
 
+func changeText(from_word, to_word):
+	var label: RichTextLabel = $Control/TextBoxContainer/TextMargin/RichTextLabel
+	label.text = label.text.replace(from_word, to_word)
+
 func add_area_on(word) -> Area2D:
+	
+	var label: RichTextLabel = $Control/TextBoxContainer/TextMargin/RichTextLabel
+	var fnt = label.get_theme_font("normal_font")
+	var fnt_size = label.get_theme_font_size("normal_font_size")
+	
+	# get position of word in text
+	var text = label.text
+	var text_lines = text.split("\n")
+	var idx = text.find(word)
+	var lines = text.count("\n")
+	var line = text.count("\n", 0, idx) if idx != 0 else 0
+	var idx_in_line = text_lines[line].find(word)
+	print(word, ", ", lines, ", ", line)
+	var length_to_word = fnt.get_string_size(text_lines[line].substr(0, idx_in_line), 0, -1, fnt_size)
+	var height_to_word = fnt.get_height(fnt_size) * line
+	var length_of_word = fnt.get_string_size(word, 0, -1, fnt_size)
+	
+	var posX = -320 + (length_of_word.x / 2.0) + length_to_word.x + $Control/ImageContainer.size.x + 65.0
+	var posY = 160 - (length_of_word.y / 2.0) - ((lines - line) * length_of_word.y)
+	
 	var area = Area2D.new()
 	var collision = CollisionShape2D.new()
 	var colShape = RectangleShape2D.new()
-	colShape.size = Vector2(50.0, 40.0)
+	colShape.size = length_of_word
 	collision.shape = colShape
 	area.add_child(collision)
-	# create Area2D
-	# Add CollisionShape
+	area.monitorable = true
+	area.position = Vector2(posX, posY)
+	area.collision_layer = 1
+	area.collision_mask = 16
+	area.set_script(area_script)
+	area.from_word = word
+	area.to_word = "hi hi"
 	return area
